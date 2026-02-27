0725350261afd41ed157a50b143a68aefa378b0f
Sundararajan R <rsundararajansundar@gmail.com>
2026-02-27 18:51:37 +0530

    Initial MERN project structure

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+// server.js
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Collaborative Coding Backend Running");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
